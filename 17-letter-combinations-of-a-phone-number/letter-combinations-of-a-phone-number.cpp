178eeaf87158b2833ca95985671a4c00983db1da
Lakshya Mudgal <161223219+LakSHyaMudgal1@users.noreply.github.com>
2026-02-10 13:03:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (9.82%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
 vector<string> ans;
     void f(int idx ,string &arr,string part , unordered_map<char, string >&mp){
-        if(idx>=arr.size()){
+        if(idx >= arr.size()){
             ans.push_back(part);
             return;
         }
+
         char ch = arr[idx];
         string str = mp[ch];
-
-        for(int i=0;i<str.size();i++){
+        
+        for(int i = 0; i < str.size(); i++){
             part.push_back(str[i]);
-            f(idx+1,arr,part,mp);
+            f(idx + 1, arr, part, mp);
             part.pop_back();
         }
 
