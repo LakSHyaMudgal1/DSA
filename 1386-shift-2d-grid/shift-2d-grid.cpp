b89a3d88acf120d27b09c7695836335f0b0f4c2b
Lakshya Mudgal <161223219+LakSHyaMudgal1@users.noreply.github.com>
2026-07-20 12:33:07 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (39.39%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.cpp
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -5,25 +5,21 @@ public:
         int n = grid[0].size();
         k = k % (n * m);
         vector<vector<int>> ans = grid;
-        while(k != 0) {
-            for(int i = 0; i < m; i++) {
-                for(int j = 0; j < n; j++) {
-                    int col = j + 1;
-                    int row = i;
-                    if(col == n){
-                        row++;
-                        col = 0;
-                    }
-                    if(row == m){
-                        row = 0;
-                    }
 
-                    ans[row][col] = grid[i][j];
-                }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int oldIndex = i * n + j;
+
+                int newIndex = (oldIndex + k) % (m * n);
+
+                int newRow = newIndex / n;
+                int newCol = newIndex % n;
+
+                ans[newRow][newCol] = grid[i][j];
             }
-            grid = ans;
-            k--;
         }
+
         return ans;
     }
 };
