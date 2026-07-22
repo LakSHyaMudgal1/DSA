59d3c292d2bbfcdc08e980cbfaeb3c48b1b342b0
Lakshya Mudgal <161223219+LakSHyaMudgal1@users.noreply.github.com>
2026-07-22 16:40:42 +0530

    Time: 14 ms (18.10%) | Memory: 27.9 MB (6.32%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    int f(int idx, bool canBuy, vector<int>& arr, vector<vector<int>> &dp){
-        if(idx == arr.size()-1){
-            if(!canBuy) return arr[idx];
-            else return 0;
+    int solve(int idx, int canBuy, int& n, vector<int>& arr, vector<vector<int>> &dp) {
+        if(idx == n - 1){
+            if(!canBuy) {
+                return arr[idx];
+            }else{
+                return 0;
+            }
         }
-        if(dp[idx][canBuy] != -1) return dp[idx][canBuy] ;
-        int pro = 0;
-        if(canBuy){
-            int pick = -arr[idx] + f(idx + 1, 0, arr, dp);
-            int notPick = f(idx + 1, 1, arr, dp);
-            pro = max(pick, notPick);
-        }else{
-            int sell = +arr[idx] + f(idx+1, 1, arr,dp);
-            int notSell = f(idx + 1, 0, arr,dp);
+
+        if(dp[idx][canBuy] != -1) return dp[idx][canBuy];
+        int pro  = 0;
+        if(canBuy) {
+            int buy = -arr[idx] + solve(idx + 1, false, n, arr, dp);
+            int notBuy = solve(idx + 1, true, n, arr, dp);
+            pro = max(buy, notBuy);
+        }
+        else{
+            int sell = +arr[idx] + solve(idx + 1, true, n, arr, dp);
+            int notSell = solve(idx + 1, false, n, arr, dp);
             pro = max(sell, notSell);
         }
         return dp[idx][canBuy] = pro;
-        
     }
     int maxProfit(vector<int>& arr) {
-        bool canBuy = true;
+        int n = arr.size();
         vector<vector<int>> dp(arr.size(),vector<int>(2,-1));
-        return f(0,canBuy,arr,dp);
+        return solve(0,true, n, arr,dp);
     }
 };
