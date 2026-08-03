d9deddbe80966124680a30fdc82066d46b7836d8
Lakshya Mudgal <161223219+LakSHyaMudgal1@users.noreply.github.com>
2026-08-03 13:22:29 +0530

    Time: 39 ms (43.17%) | Memory: 142.4 MB (31.45%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
+    int solve(int idx, int n, vector<int>& arr, vector<int>& dp) {
+        if(idx >= n) return 0;
+
+        int t1 = INT_MIN;
+
+        if(dp[idx] != INT_MIN) return dp[idx];
+
+        t1 = arr[idx] - solve(idx + 1, n, arr, dp);
+        if(idx + 1 < n) t1 = max(t1, arr[idx] + arr[idx + 1] - solve(idx + 2, n, arr, dp));
+        if(idx + 2 < n) t1 = max(t1, arr[idx] + arr[idx + 1] + arr[idx + 2] - solve(idx + 3, n, arr, dp));
+
+        return dp[idx] = t1;
+        }
     string stoneGameIII(vector<int>& arr) {
         int n = arr.size();
+        vector<int> dp(n, INT_MIN);
 
-        vector<int> dp(n + 1, 0);
+        int ans = solve(0, n, arr, dp);
 
-        for(int i = n - 1; i >= 0; i--) {
-            int take = 0;
-            dp[i] = INT_MIN;
-
-            for(int k = 0; k < 3 && i + k < n; k++) {
-                take += arr[i + k];
-                dp[i] = max(dp[i], take - dp[i + k + 1]);
-            }
-        }
-
-        if(dp[0] > 0) return "Alice";
-        if(dp[0] == 0) return "Tie";
+        if(ans > 0) return "Alice";
+        else if(ans == 0) return "Tie";
         return "Bob";
     }
 };
