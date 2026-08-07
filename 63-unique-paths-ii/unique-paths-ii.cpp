3dd5373db5517649d020a154bc789f28bdefface
Lakshya Mudgal <161223219+LakSHyaMudgal1@users.noreply.github.com>
2026-08-07 15:46:16 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (72.84%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& arr, vector<vector<int>>& dp) {
-        if(i < 0 || j < 0) return 0;
-        if(arr[i][j] == 1) return 0;
-
-        if(i == 0 && j == 0) return 1;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int left = solve(i, j - 1, arr, dp);
-        int top = solve(i - 1, j, arr, dp);
-
-        return dp[i][j] = left + top;
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& arr) {
         int m = arr.size();
         int n = arr[0].size();
-        vector<vector<int>>dp(m, vector<int>(n, -1));
-        return solve(m - 1, n - 1, arr, dp);
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        if(arr[0][0] == 1) return 0;
+
+        dp[0][0] = 1;
+
+        for(int i = 1; i < m; i++) {
+            if(arr[i][0] == 0)
+                dp[i][0] = dp[i - 1][0];
+        }
+
+        for(int j = 1; j < n; j++) {
+            if(arr[0][j] == 0)
+                dp[0][j] = dp[0][j - 1];
+        }
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(arr[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 };
