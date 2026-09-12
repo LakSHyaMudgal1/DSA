2b06f6aa3876a1ec14d208120eeadfc6e1cbb71f
Lakshya Mudgal <161223219+LakSHyaMudgal1@users.noreply.github.com>
2026-09-12 12:59:38 +0530

    Time: 970 ms (47.82%) | Memory: 334.1 MB (36.95%) - LeetSync

--- 3562-maximum-score-of-non-overlapping-intervals/maximum-score-of-non-overlapping-intervals.cpp
+++ 3562-maximum-score-of-non-overlapping-intervals/maximum-score-of-non-overlapping-intervals.cpp
@@ -1,82 +1,34 @@
 class Solution {
 public:
-    struct Node {
-        long long score;
-        vector<int> ids;
-        bool valid;
-
-        Node() : score(0), valid(false) {}
-        Node(long long s, vector<int> v) : score(s), ids(std::move(v)), valid(true) {}
-    };
-
-    bool better(const Node& a, const Node& b) {
-        if (!a.valid) return false;
-        if (!b.valid) return true;
-
-        if (a.score != b.score) return a.score > b.score;
-
-        return a.ids < b.ids;
-    }
-
     vector<int> maximumWeight(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        const int K = 4;
-
-        vector<array<long long, 4>> a(n);
-
-        for (int i = 0; i < n; ++i) {
-            a[i] = {intervals[i][0], intervals[i][1], intervals[i][2], i};
+        int n=intervals.size();
+        vector<vector<int>> a(n,vector<int>(4,0));
+        for(int i=0;i<n;i++){
+            a[i][0]=intervals[i][0];
+            a[i][1]=intervals[i][1];
+            a[i][2]=intervals[i][2];
+            a[i][3]=i;
         }
-
-        sort(a.begin(), a.end(), [](const auto& x, const auto& y) {
-            return x[1] < y[1];
-        });
-
-        vector<long long> ends(n);
-        for (int i = 0; i < n; ++i) {
-            ends[i] = a[i][1];
-        }
-
-        vector<vector<Node>> dp(K + 1, vector<Node>(n + 1));
-
-        for (int i = 0; i <= n; ++i) {
-            dp[0][i] = Node(0, {});
-        }
-
-        for (int i = 1; i <= n; ++i) {
-            long long l = a[i - 1][0];
-            long long w = a[i - 1][2];
-            int idx = (int)a[i - 1][3];
-
-            int p = lower_bound(ends.begin(), ends.begin() + (i - 1), l) - ends.begin();
-
-            for (int k = 1; k <= K; ++k) {
-                dp[k][i] = dp[k][i - 1];
-
-                if (dp[k - 1][p].valid) {
-                    vector<int> ids = dp[k - 1][p].ids;
-
-                    ids.push_back(idx);
-
-                    sort(ids.begin(), ids.end());
-
-                    Node take(dp[k - 1][p].score + w, std::move(ids));
-
-                    if (better(take, dp[k][i])) {
-                        dp[k][i] = std::move(take);
-                    }
+        sort(a.begin(),a.end());
+        vector<int> ls(n,0);
+        for(int i=0;i<n;i++) ls[i]=a[i][0];
+        vector<vector<long long>> s(n+1,vector<long long>(5,0));
+        vector<vector<vector<int>>> p(n+1,vector<vector<int>>(5));
+        for(int i=n-1;i>=0;i--){
+            int nx=upper_bound(ls.begin(),ls.end(),a[i][1])-ls.begin();
+            for(int k=1;k<=4;k++){
+                s[i][k]=s[i+1][k];
+                p[i][k]=p[i+1][k];
+                long long t=(long long)a[i][2]+s[nx][k-1];
+                vector<int> q=p[nx][k-1];
+                q.push_back(a[i][3]);
+                sort(q.begin(),q.end());
+                if(t>s[i][k] || (t==s[i][k] && q<p[i][k])){
+                    s[i][k]=t;
+                    p[i][k]=q;
                 }
             }
         }
-
-        Node ans;
-
-        for (int k = 1; k <= K; ++k) {
-            if (better(dp[k][n], ans)) {
-                ans = dp[k][n];
-            }
-        }
-
-        return ans.ids;
+        return p[0][4];
     }
 };
